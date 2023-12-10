1d2955aa9e593d74b764eda5aea89dc91209daab
Reem-ousama <reem.ziadeh10063@gmail.com>
2023-12-10 17:53:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,226 @@
+/*1*/
+// var num=Number(prompt('enter your number'));
+// console.log(num);
+
+/*2*/
+// var x=Number(prompt('enter number 1'));
+
+// if(x%3==0 && x%4==0){
+//     console.log('yes');
+// }else {console.log('no')};
+
+/*3*/
+// var x=Number(prompt('enter number 1'));
+// var y=Number(prompt('enter number 2'));
+
+// if(x>y){
+//     console.log(x);
+// }else if(x<y){
+//     console.log(y);
+// };
+
+/*4*/
+// var x=Number(prompt('enter your number'));
+
+// if(x<0){
+//     console.log('negative');
+// }else{console.log('positive')};
+
+/*5*/
+// var x=Number(prompt('enter number 1'));
+// var y=Number(prompt('enter number 2'));
+// var z=Number(prompt('enter number 3'));
+
+// if(x>y && x>z ){
+//     console.log('max element='+x);
+// }else if(y>x && y>z){
+//     console.log('max element='+y);
+// }else if(z>x && z>y){
+//     console.log('max element='+z);
+// };
+// if(x<y && x<z ){
+//     console.log('min element='+x);
+// }else if(y<x && y<z){
+//     console.log('min element='+y);
+// }else if(z<x && z<y){
+//     console.log('min element='+z);
+// };
+
+/*6*/
+// var x=Number(prompt('enter number 1'));
+// if(x%2==0){
+//     console.log('even')
+// }else {
+//     console.log('odd')
+// };
+
+/*8*/
+// var x=prompt('enter the letter');
+//  if(x=='a' || x=='e' || x=='l'  || x=='o' || x=='u'){
+//      console.log('vowel')
+//  }else {
+// console.log('Consonant')
+//  };
+
+/*9*/
+// var x=Number(prompt('enter number'));
+
+// for(var i=1 ; i<=x ;i++ ){
+//     console.log(i)
+// }
+
+/*10*/
+// var x=Number(prompt('enter number'));
+// for(var i=1; i<=12; i++){
+//     console.log(i*x)
+// };
+
+/*11*/
+// var x=Number(prompt('enter number'));
+
+// for( var i=1 ; i<=x ; i++){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+// }
+
+/*12 */
+// var x=Number(prompt('enter your mark of suject 1'));
+// var y=Number(prompt('enter your mark of suject 2'));
+// var z=Number(prompt('enter your mark of suject 3'));
+// var n=Number(prompt('enter your mark of suject 4'));
+// var m=Number(prompt('enter your mark of suject 5'));
+// var sum=x+y+z+n+m
+// var avre=sum/5
+// var pre=(sum%100)
+// for(var i=0; i<5; i++){
+//     console.log(sum , avre ,pre)
+   
+// }
+
+// var x=Number(prompt('enter num 1'));
+// var y=Number(prompt('enter num 2'));
+
+// for(var i=0 ; i<=y ;i++){
+//     console.log(x^y)
+// }
+
+/*13*/
+//  var month=prompt('enter the number of month')
+// switch(month){
+//     case '1':console.log('Days in month:31') ; break;
+//     case '2':console.log('Days in month:28') ; break;
+//     case '3':console.log('Days in month:30') ; break;
+//     case '4':console.log('Days in month:31') ; break;
+//     case '5':console.log('Days in month:30') ; break;
+//     case '6':console.log('Days in month:31') ; break;
+//     case '7':console.log('Days in month:30') ; break;
+//     case '8':console.log('Days in month:31') ; break;
+//     case '9':console.log('Days in month:30') ; break;
+//     case '10':console.log('Days in month:31') ; break;
+//     case '11':console.log('Days in month:30') ; break;
+//     case '12':console.log('Days in month:31') ; break;
+// }
+
+/*14*/ 
+// var score=Number(prompt('enter you score'))
+
+// if(score <= 100 && score >= 90){
+//      console.log('Grad A')
+//  }
+//  else if(score < 90 && score >= 80){
+//      console.log('Grad B')
+//  }
+//  else if(score < 80 && score >= 70){
+//      console.log('Grad C')
+//  }
+//  else if(score < 70 && score >= 60){
+//      console.log('Grad D')
+//  }
+//  else if(score < 60 && score >= 40){
+//      console.log('Grad E')
+//  }else if(score < 40)
+//     {console.log('Grad F')}
+
+
+/*15 */
+// var month=prompt('enter the number of month')
+// switch(month){
+//     case '1':console.log('Days in month:31') ; break;
+//     case '2':console.log('Days in month:28') ; break;
+//     case '3':console.log('Days in month:30') ; break;
+//     case '4':console.log('Days in month:31') ; break;
+//     case '5':console.log('Days in month:30') ; break;
+//     case '6':console.log('Days in month:31') ; break;
+//     case '7':console.log('Days in month:30') ; break;
+//     case '8':console.log('Days in month:31') ; break;
+//     case '9':console.log('Days in month:30') ; break;
+//     case '10':console.log('Days in month:31') ; break;
+//     case '11':console.log('Days in month:30') ; break;
+//     case '12':console.log('Days in month:31') ; break;
+// }
+
+/*16 */
+
+// var weth=prompt('enter the letter')
+
+// switch(weth){
+//     case 'u':console.log(' vowel');break;
+//     case 'a':console.log(' vowel');break;
+//     case 'e':console.log(' vowel');break;
+//     case 'i':console.log(' vowel');break;
+//     case 'o':console.log(' vowel');break;
+//     default:console.log('consonant');
+// }
+
+/*17 */
+// var num1=Number(prompt('enter number 1'));
+// var num2=Number(prompt('enter number 2'));
+
+// switch(true) {
+//     case (num1 > num2):
+//         console.log('num 1 is a max');
+//         break;
+//     case (num1 < num2):
+//         console.log('num 2 is a max');
+//         break;
+//      default: console.log(false);
+// }
+
+/*18 */
+// var x=Number(prompt('enter number 1'));
+// switch(true){
+//     case (x%2==0):console.log('even') ; break;
+//     case (x%2!==0):console.log('odd');
+// }
+
+/*19 */
+// var x=Number(prompt('enter number 1'));
+// switch(true){
+//     case (x<0):console.log('negative') ; break;
+//      case (x>0):console.log('positive'); break;
+//      default:console.log('zero');
+//  }
+
+/*20 */
+//  var a=Number(prompt('enter number 1'));
+//  var b=Number(prompt('enter number 2'));
+// var tok=prompt('enter the method') ;
+// var x=(a+b)
+// var y=(a-b)
+// var z=(a*b)
+// var n=(a%b)
+//  switch(true){
+//     case (tok=='+'):{
+//         console.log(x);
+//     }; break;
+//     case (tok=='-'):{
+//         console.log(y);
+//     }; break;
+//     case (tok=='*'):{
+//         console.log(z);
+//     }; break;
+//     case (tok=='%'):{
+//         console.log(n);
+//     }; break;
+//  }
